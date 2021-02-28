4fb5ef4474c61d29a2c6bda9f703f189c4a61eea
VITOR_HUGO GOMES <vitorhgfluiz@gmail.com>
2021-02-28 17:51:04 -0300

    Day Four Complete

---
+++ src/components/Countdown.tsx
@@ -0,0 +1,58 @@
+import { useState, useEffect, useContext } from 'react';
+import { CountdownContext } from '../contexts/CountdonwContext'
+import { ChallengesContext } from '../contexts/ChallengesContext';
+import styles from '../styles/components/Countdown.module.css'
+
+
+export function Countdown(){
+    const { minutes, seconds, hasFinished, isActive, startCountdown, resetCountdown, } =  useContext(CountdownContext)
+
+
+    const [minutesLeft, minutesRight]  = String(minutes).padStart(2, '0').split('')
+    const [secondsLeft, secondsRight]  = String(seconds).padStart(2, '0').split('')   
+
+    return(
+        <div>
+            <div className={styles.countdownContainer}>
+                <div>
+                    <span>{minutesLeft}</span>
+                    <span>{minutesRight}</span>
+                </div>
+                <span>:</span>
+                <div>
+                    <span>{secondsLeft}</span>
+                    <span>{secondsRight}</span>
+                </div>
+            </div>
+
+            { hasFinished ? (
+                <button
+                    disabled 
+                    className={styles.countdownButton}
+                >
+                    Ciclo encerrado
+                </button>
+            ) : <>
+            
+            { isActive ? (
+                <button 
+                    type="button" 
+                    className={`${styles.countdownButton} ${styles.countdownButtonActive}`}
+                    onClick={resetCountdown}
+                >
+                    Abandonar ciclo
+                </button>
+            ) : (
+                <button 
+                    type="button" 
+                    className={styles.countdownButton}
+                    onClick={startCountdown}
+                >
+                    Iniciar um ciclo
+                </button>
+            ) }            
+            </> 
+            }
+        </div>
+    );
+}
